cd9d78c2f48a5bcfb91d94804d3e2fa033e1f94f
Takashi Ando <dodo5522@gmail.com>
2021-02-18 23:58:33 +0900

    remove zenkaku-font refs #769

--- misakiUTF16.h
+++ misakiUTF16.h
@@ -12,7 +12,7 @@
 #include <avr/pgmspace.h>
 #include <Arduino.h>
 
-#define FTABLESIZE     1000      // フォントテーブルデータサイズ
+#define FTABLESIZE     94        // フォントテーブルデータサイズ
 #define FONT_LEN       8         // 1フォントのバイト数
 
 extern PROGMEM const uint8_t fdata[];						// フォントデータ
